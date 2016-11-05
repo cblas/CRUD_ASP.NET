171fb73a5864fcd84c316e08628b9449e06a1ee8
Christian Blas Sánchez <christian.blas28@gmail.com>
2016-11-04 22:34:08 -0600

    Se agrega proyecyo .NET

---
+++ WebApplication_Test/WebApplication_Test/Data/Alumno.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication_Test.Data
+{
+    public class Alumno: Persona
+    {
+        private string matricula = "";
+        private string grado = "";
+
+        public string Matricula {
+            get { return matricula; }
+            set { matricula = value; }
+        }
+
+        public string Grado {
+            get { return grado; }
+            set { grado = value; }
+        }
+    }
+}
---
+++ WebApplication_Test/WebApplication_Test/Data/Persona.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication_Test.Data
+{
+    public class Persona
+    {
+        protected string nombre = "";
+        protected string aPaterno = "";
+        protected string aMaterno = "";
+        protected int edad = 0;
+
+        public string Nombre {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public string APaterno {
+            get { return aPaterno; }
+            set { aPaterno = value; }
+        }
+
+        public string AMaterno {
+            get { return aMaterno; }
+            set { aMaterno = value; }
+        }
+
+        public int Edad {
+            get { return edad; }
+            set { edad = value; }
+        }
+    }
+}
