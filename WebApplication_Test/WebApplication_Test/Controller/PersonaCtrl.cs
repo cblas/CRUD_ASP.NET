83351b6e9231144dd072418eac6a755a32f9a99b
cblasia <cblas@ia.com.mx>
2017-10-19 12:13:32 -0500

    Se sube nuevo controlador persona.

---
+++ WebApplication_Test/WebApplication_Test/Controller/PersonaCtrl.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication_Test.Controller
+{
+    public class PersonaCtrl
+    {
+        public string nombre {get; set;}
+        public string apaterno { get; set; }
+    }
+}
